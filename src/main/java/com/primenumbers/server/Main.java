25d262f99e32fad10fa936d872ecc7371039841d
Ali Araghi <ali.araghi@gmail.com>
2020-12-17 11:52:46 +0100

    Update main method to use gRPC method

--- src/main/java/com/primenumbers/server/Main.java
+++ src/main/java/com/primenumbers/server/Main.java
@@ -3,39 +3,50 @@ package com.primenumbers.server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.primenumbers.server.directives.Directives;
-import com.primenumbers.server.series.PrimeNumbersProtocol;
+import akka.actor.ActorSystem;
+import akka.http.javadsl.*;
+import akka.stream.ActorMaterializer;
+import akka.stream.Materializer;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
-import akka.actor.typed.ActorSystem;
-import akka.actor.typed.Behavior;
-import akka.actor.typed.Terminated;
-import akka.actor.typed.javadsl.Behaviors;
+import com.primenumbers.server.grpc.*;
+
+import java.util.concurrent.CompletionStage;
 
 public class Main {
-    
+
     private static final Logger log = LoggerFactory.getLogger(Main.class);
+    
     public static void main(String[] args) {
+        // important to enable HTTP/2 in ActorSystem's config
+        Config conf = ConfigFactory.parseString("akka.http.server.preview.enable-http2 = on")
+            .withFallback(ConfigFactory.defaultApplication());
+
+        // Akka ActorSystem Boot
+        ActorSystem sys = ActorSystem.create("PrimeNumberServer", conf);
+
         try {
-            // Create the main ActorSystem
-            //ctorSystem.create(Main.create(), "primenumbers-server");
-        } catch (Exception x) {
-            log.error("Server Stoped with an error: {}", x);
-            System.exit(1);
+            run(sys).thenAccept(binding -> {
+                System.out.println("gRPC server bound to: " + binding.localAddress());
+            });
+        } catch (Exception e) {
+            log.error("System Error: {}", e);
         }
+
+        // ActorSystem threads will keep the app alive until `system.terminate()` is called
         log.info("prime-number-server started");
     }
-//
-//    private static Behavior<Void> create() {
-//        return Behaviors.setup(context -> {
-//            // Setup the protocols and directives
-//            PrimeNumbersProtocol primeNumbersProtocol = PrimeNumbersProtocol.create(context);
-//            new Directives(primeNumbersProtocol, 8080);
-//            log.debug("Main Actor Ref:, {}", context.getSelf());
-//            return Behaviors.receive(Void.class)
-//                .onSignal(Terminated.class, sig -> Behaviors.stopped())
-//                .build();
-//        });
-//    }
+
+    public static CompletionStage<ServerBinding> run(ActorSystem sys) throws Exception {
+        Materializer mat = ActorMaterializer.create(sys);
+
+        // Instantiate implementation
+        PrimeNumbersService impl = new PrimeNumbersServiceImpl(mat);
+
+        return Http.get(sys).bindAndHandleAsync(
+            PrimeNumbersServiceHandlerFactory.create(impl, sys),
+            ConnectHttp.toHost("127.0.0.1", 8090), 
+            mat);
+    }
 }
